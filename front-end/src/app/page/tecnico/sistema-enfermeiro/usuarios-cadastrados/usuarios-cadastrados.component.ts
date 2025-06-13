eb18292f89caab07d405aa57df187cd5d8a50160
Jorge Gabriel Rodrigues Ferreira <jorgegabrielrf@gmail.com>
2025-06-13 04:37:47 -0300

    Front-end - Geração e reestrutuação da tela de enfermeiro, lista de pacientes cadastrados finalizado.

--- front-end/src/app/page/tecnico/sistema-enfermeiro/usuarios-cadastrados/usuarios-cadastrados.component.ts
+++ front-end/src/app/page/tecnico/sistema-enfermeiro/usuarios-cadastrados/usuarios-cadastrados.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-usuarios-cadastrados',
-  imports: [],
+  imports: [InputTextModule, FormsModule, FloatLabelModule, ButtonModule],
   templateUrl: './usuarios-cadastrados.component.html',
   styleUrl: './usuarios-cadastrados.component.scss'
 })
 export class UsuariosCadastradosComponent {
-
+  
 }
