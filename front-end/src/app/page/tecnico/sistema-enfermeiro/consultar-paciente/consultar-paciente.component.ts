ef4a5893c32fa5734029fa8ba8d3cdef5bb47c3d
Jorge Gabriel Rodrigues Ferreira <jorgegabrielrf@gmail.com>
2025-06-13 02:58:54 -0300

    Front-end - Geração e reestrutuação da tela de enfermeiro, consulta de paciente finalizado.

--- front-end/src/app/page/tecnico/sistema-enfermeiro/consultar-paciente/consultar-paciente.component.ts
+++ front-end/src/app/page/tecnico/sistema-enfermeiro/consultar-paciente/consultar-paciente.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
   selector: 'app-consultar-paciente',
-  imports: [],
+  imports: [ButtonModule, SelectButtonModule, FormsModule, InputMaskModule, InputTextModule],
   templateUrl: './consultar-paciente.component.html',
   styleUrl: './consultar-paciente.component.scss'
 })
 export class ConsultarPacienteComponent {
+  podeEditar: boolean = false;
 
+  alternarEdicao() {
+    this.podeEditar = !this.podeEditar;
+  }
+
+  stateOptions = [
+    { label: 'Histórico', value: true },
+    { label: 'Pendentes', value: false }
+  ];
+
+  value: boolean = true;
+
+  // Função para buscar dados conforme a opção selecionada
+  buscarDados() {
+    if (this.value == true) {
+      console.log("Buscando dados de Histórico..."); // Buscar vacinas realizada
+    } else {
+      console.log("Buscando dados de Pendentes..."); // Buscar vacinas pendentes
+    }
+  }
 }
