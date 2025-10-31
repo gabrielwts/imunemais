d74b5a54e4837cb3d807e5f476dce4fac19db3f2
Jorge Gabriel Rodrigues Ferreira <jorgegabrielrf@gmail.com>
2025-10-31 01:07:56 -0300

    [F&B] Recuperação de Senha - Script enviar e-mail com código (75%)

---
+++ front-end/src/app/models/recuperar-senha-cadastro.spec.ts
@@ -0,0 +1,7 @@
+import { RecuperarSenhaCadastro } from './recuperar-senha-cadastro';
+
+describe('RecuperarSenhaCadastro', () => {
+  it('should create an instance', () => {
+    expect(new RecuperarSenhaCadastro()).toBeTruthy();
+  });
+});
---
+++ front-end/src/app/models/recuperar-senha-cadastro.ts
@@ -0,0 +1,3 @@
+export class RecuperarSenhaCadastro {
+    constructor(public senha: string = ""){}
+}
